dfe0ed2333222d1c336ff5150a942deac2902074
OlegRudevich <olegrudevich19@gmail.com>
2019-09-12 02:41:25 +0300

    init: generate Spring Boot project using Spring's generator(start.spring.io).

---
+++ src/main/java/com/gmail/olegrudevich19/SMovie/SMovieApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.olegrudevich19.SMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SMovieApplication.class, args);
+	}
+
+}
